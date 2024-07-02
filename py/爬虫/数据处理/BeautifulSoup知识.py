cc09107898403f574503644dd272a2d5936c6987
ryoo2001 <txong123@gmail.com>
2024-07-02 20:20:46 +0800

    格式修改

--- py/爬虫/数据处理/BeautifulSoup知识.py
+++ py/爬虫/数据处理/BeautifulSoup知识.py
@@ -12,7 +12,7 @@ soup1 = BeautifulSoup(open('test.html'))
 ## find_all( name , attrs , recursive , text , **kwargs )
 ### name参数
 soup.find_all("h1") # 查找文档中h1标签内容
-soup.find_all(["h1", "h2"]) # 查找文档中h1和h2标签内容
+soup.find_all(["h1","h2"]) # 查找文档中h1和h2标签内容
 ### keyword参数
 soup.find_all("h1",class_="Special" ) #查找h1标签中class_="Special" 内容
 soup.find_all("div",class_="item") #查找div标签中class_="item"内容
