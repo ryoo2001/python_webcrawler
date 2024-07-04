89616d0563705b57c52af69ab2f8a13cdc447e3a
ryoo2001 <txong123@gmail.com>
2024-07-04 10:49:16 +0800

    新增注释

--- python/crawler/Main_music_soup.py
+++ python/crawler/Main_music_soup.py
@@ -51,21 +51,23 @@ def get_data(baseurl):
             res = {}
 
             # 获取音乐的链接
-            res['link'] =  item.find_all("a")[0]['href']
+            res['link'] =  item.find_all("a")[0]['href'] # 获取标签a的所有内容，在取第一个a标签内的href标签内容
             # 获取图片的链接
-            res['imgsrc'] = item.find_all("img")[0]['src']
-            # # 获取音乐的名字
-            title = item.select(".pl2 > a")[0]
-            res['title'] = next(title.children).strip()
-            alt_title = title.select('span')
+            res['imgsrc'] = item.find_all("img")[0]['src'] # 获取标签img的所有内容，在取第一个img标签内的src标签内容
+            # 获取音乐的名字
+            title = item.select(".pl2 > a")[0] # 获取clss=pl2，子标签为a的内容
+            res['title'] = next(title.children).strip() # 处理直接在a标签下的部分音乐名
+            alt_title = title.select('span') # 获取大部分在span标签内的音乐名
             if alt_title:
-                res['alt_title'] = alt_title[0].get_text().strip()
-
-            res['rate'] = item.select(".rating_nums")[0].get_text().strip()
-            rate_count_el = item.select(".star > span.pl")[0]
+                res['alt_title'] = alt_title[0].get_text().strip() # 使用get_text提取不含标签信息的文本,并使用strip去除头尾空格
+            # 获取评分
+            res['rate'] = item.select(".rating_nums")[0].get_text().strip() # 查找class=rating_nums内容，get_text提取不含标签信息的文本,并使用strip去除头尾空格
+            # 获取评价人数
+            rate_count_el = item.select(".star > span.pl")[0] # 查找clss=star为父标签中的<span class="pl">子标签
             res['rate_count'] = re.findall(r'\d+', rate_count_el.get_text())[0]
-            info_el = item.select("p.pl")[0]
-            res['artists'] = info_el.get_text().split("/")[0].strip()
+            # 获取歌手名字
+            info_el = item.select("p.pl")[0] # 查找以p标签class=pl的内容
+            res['artists'] = info_el.get_text().split("/")[0].strip() # 使用get_text提取不含标签信息的文本，/为分隔符后取第一个元素，去除首尾空格
             print(res)
             # 每一个音乐的信息存入到datalist里面
             datalist.append(res)
