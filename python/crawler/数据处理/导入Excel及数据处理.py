a0f9d2df9ee6fafc4a2c243f0c1bcbb47e0453a5
ryoo2001 <txong123@gmail.com>
2024-07-05 10:28:45 +0800

    新增数据处理知识，修改文件名

--- python/crawler/数据处理/导入Excel及数据处理.py
+++ python/crawler/数据处理/导入Excel及数据处理.py
@@ -18,14 +18,15 @@ for row in range(sheet.nrows): # 行循环，读取行数量
         cell_value = sheet.cell_value(row,col) # 读取单元格信息
         # print(f"{row + 1},{col + 1}单元格值为:{cell_value}")
 
+
 # 创建新的Excel工作表
 excel_book = xlwt.Workbook(encoding="utf-8")
-new_excel = excel_book.add_sheet("sheet1", cell_overwrite_ok=True)
+new_excel = excel_book.add_sheet("sheet1", cell_overwrite_ok=True) # cell_overwrite_ok单元格内容覆写开关
 
 # 写入数据
 new_excel.write(0, 0, "hello")
 new_excel.write(1, 0, "word")
 
 # 保存数据
-savepatch = r"python\crawler"
-excel_book.save(savepatch) #保存文件到savepatch目录下
+savepatch = "python/crawler/test.xls"
+excel_book.save(savepatch) #保存文件到savepatch目录下,文件名为:test
